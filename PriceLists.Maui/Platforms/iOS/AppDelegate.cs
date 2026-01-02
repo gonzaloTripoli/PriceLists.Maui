fb0ed6b54da9e048fca0fb2ca8f6b628ed2d2cf8
Gonzalo Tripoli <gonzalotripoli1@hotmail.com>
2026-01-02 17:06:41 -0300

    Agregar archivos de proyecto.

---
+++ PriceLists.Maui/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace PriceLists.Maui
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
